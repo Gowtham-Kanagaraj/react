3eb60c06a4eb93bb2107840fafd941098c69c797
Gowtham <gowtham.kanagaraaj@gmail.com>
2023-08-03 22:03:58 +0530

    Day 3 notes added

--- hello/src/App.js
+++ hello/src/App.js
@@ -2,23 +2,19 @@ import './App.css';
 import Employee from './components/Employee';
 
 function App() {
+
+  console.log("we are showing employee component")
+  const showEmployees = false;
   return (
     <div className="App">
-      <header className="App-header">
-        
+
+      {showEmployees ? (
+        <>
         <Employee />
-        {/* <p>
-         Hello
-        </p> */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        </>
+      ): (<p> You cannot view employee </p>
+      )}
+    
     </div>
   );
 }
